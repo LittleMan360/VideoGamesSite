1f3b75ec9cd75927bad900764070ad8b10a2aa76
LittleMan360 <87342719+LittleMan360@users.noreply.github.com>
2025-12-05 16:21:58 -0500

    Updated now playing page to be more user friendly and added database for now playing games

--- VideoGamesSite/Migrations/20251205211923_AddNowPlayingItems.Designer.cs
+++ VideoGamesSite/Migrations/20251205211923_AddNowPlayingItems.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoGamesSite.Data;
 
@@ -10,9 +11,11 @@ using VideoGamesSite.Data;
 namespace VideoGamesSite.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251205211923_AddNowPlayingItems")]
+    partial class AddNowPlayingItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
